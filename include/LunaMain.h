74ebacbcd93058dcc6e1688d64eec8ac89234724
mauvecow <mauve@sandwich.net>
2015-04-26 15:52:38 -0700

    LunaMain now compatible with Luna & Win32 Console.

--- include/LunaMain.h
+++ include/LunaMain.h
@@ -1,4 +1,4 @@
-/*
+﻿/*
   DxPortLib - A portability library for DxLib-based software.
   Copyright (C) 2013-2015 Patrick McCarthy <mauve@sandwich.net>
 
@@ -19,13 +19,15 @@
   3. This notice may not be removed or altered from any source distribution.
  */
 
-/* Replicates the Luna's Main functionality. Include in your main.cpp
- * equivalent as follows:
-
-#ifdef DXPORTLIB_LUNA
-#  include "LunaMain.h"
-#endif
-
+/* Replicates the Luna's Main functionality.
+ * 
+ * To use, do the following:
+ * - Replace Luna::Main with LUNAMAIN
+ * - Replace Luna::Init with LUNAINIT
+ * - Replace Luna::MessageProc with LUNAMESSAGEPROC
+ *
+ * Compatible with both standard Luna and DxPortLib Luna.
+ *
  * If you're doing another platform with a different entrypoint, replace
  * as needed.
  */
@@ -35,7 +37,7 @@
 
 #include "Luna.h"
 
-#ifdef DXPORTLIB_LUNA_INTERFACE
+#if defined(DXPORTLIB_LUNA)
 
 /* DxPortLib does not use LunaMessageProc. */
 
@@ -47,21 +49,27 @@ extern Bool LunaInit();
 extern void LunaMain(Sint32 argc, char *argv[]);
 extern Bool LunaMessageProc( Sint32 Msg, Sint32 wParam, Sint32 lParam );
 
-#ifdef WIN32
+#ifdef _WIN32
 /* for _argc/_argv */
 #include <stdlib.h>
 
-/* WinMain */
+/* WinMain - Used for non-Console build so stacking with main() is okay */
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrev, LPSTR Cmd, INT Show) {
     return Luna::BootMain(__argc, __argv, &LUNAINIT, &LUNAMAIN, &LUNAMESSAGEPROC);
 }
-#else
+#endif
+
 /* main() */
 int main(int argc, char **argv) {
     return Luna::BootMain(argc, argv, &LUNAINIT, &LUNAMAIN, &LUNAMESSAGEPROC);
 }
-#endif
 
-#endif /* #ifdef DXPORTLIB_LUNA_INTERFACE */
+#else
+
+#define LUNAMAIN Luna::Main
+#define LUNAINIT Luna::Init
+#define LUNAMESSAGEPROC Luna::MessageProc
+
+#endif /* #if defined(___LUNA_DX9___) */
 
 #endif /* #define LUNAPORTLIB_MAIN_H */
